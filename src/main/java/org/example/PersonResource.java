ab271f90247ecbd5126d9c3b107c834e51c92379
Marco Buri <marco.buri@qumea.com>
2024-02-05 10:22:24 +0100

    register proto classes for reflection

--- src/main/java/org/example/PersonResource.java
+++ src/main/java/org/example/PersonResource.java
@@ -1,15 +1,19 @@
 package org.example;
 
 import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.Timestamp;
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.example.raw.Person;
+import org.example.raw.PersonProto;
 import org.jboss.logging.Logger;
 
 import java.util.Optional;
 
 @Path("/person")
+@RegisterForReflection(targets = {Person.class, PersonProto.class, Person.Builder.class, com.google.protobuf.Timestamp.class, Timestamp.Builder.class})
 public class PersonResource {
 
     private static final Logger LOG = Logger.getLogger(PersonResource.class);
